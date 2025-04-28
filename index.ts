b67025bacfa5175318dc60b6dbc6294b3c707fe3
Allen <valternatean13@gmail.com>
2025-04-28 08:10:16 +0700

    chore: change endpoint base and update tsconfig

--- index.ts
+++ index.ts
@@ -1,19 +1,23 @@
 import "tsconfig-paths/register";
-import express from "express";
+import express, { Router } from "express";
 import { gEnv } from "./env.ts";
 import { ErrorHandler } from "src/_shared/middlewares/ErrorHandler.ts";
-import { routerV1 } from "@UserManagement/routes/index.ts";
-
-const APP = express()
-const timeBegin = Date.now()
-const errorHandler = new ErrorHandler()
+import { routerV1 as userManagementRouter } from "@UserManagement/routes/index.ts";
+import { administrationRouterV1 } from "@Administration/routes/index.ts";
 
 // Set app settings
 if(gEnv.DEV_ON) {
     Error.stackTraceLimit = 30
 }
 
+const timeBegin = Date.now()
+const errorHandler = new ErrorHandler()
 
+const routerV1 = Router()
+routerV1.use("/account", userManagementRouter)
+routerV1.use("/administration", administrationRouterV1)
+
+const APP = express()
 APP.use(express.json())
 APP.use(express.urlencoded({extended: false}))
 APP.use( "/api/v1", routerV1)
@@ -27,7 +31,5 @@ APP.use( "/", (_, res) => {
 )
 APP.use(errorHandler.handle.bind(errorHandler))
 
-APP.listen(
-    gEnv.PORT, 
-    () => {console.log(`Server's up and running on :${gEnv.PORT}`)})
+APP.listen( gEnv.PORT, () => {console.log(`Server's up and running on :${gEnv.PORT}`)})
 
